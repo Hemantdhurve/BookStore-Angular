bc100ef19198fc482c0b6bdfad5de839b12704b0
Hemantdhurve <hemantdhurve007@gmail.com>
2023-02-10 20:10:17 +0530

    [Hemant] Added test case for the mycart component

--- src/app/components/mycart/mycart.component.spec.ts
+++ src/app/components/mycart/mycart.component.spec.ts
@@ -2,12 +2,15 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+import { CartserviceService } from '../Services/cartService/cartservice.service';
 
 import { MycartComponent } from './mycart.component';
 
 describe('MycartComponent', () => {
   let component: MycartComponent;
   let fixture: ComponentFixture<MycartComponent>;
+  let service:CartserviceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,9 +28,19 @@ describe('MycartComponent', () => {
     fixture = TestBed.createComponent(MycartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(CartserviceService)
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //Test case 1
+  it('should check spyon service works', () => {
+    spyOn(service,'getCartDetails').and.returnValue(of());
+    let getcart=component.getCartDetails();
+    expect(service.getCartDetails).toHaveBeenCalled();
+  });
+  
 });
